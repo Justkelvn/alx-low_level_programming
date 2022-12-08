f520d146d50a0ba26d560053080de63e459b158c
justkelvn <olowodepraise@gmail.com>
2022-12-08 15:06:09 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main -Entry
+ * main - Entry
  * Return: Always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);)
+	puts("\"Programming is like building a mutlilingual puzzle);
+	return (0);
 }
