fab2eb7485a2fd066021748280e6c05580e9d13e
justkelvn <olowodepraise@gmail.com>
2022-12-08 14:50:24 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,5 +5,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);)
+}
