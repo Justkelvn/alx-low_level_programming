ae878b1d4721c0eee368bdcb5ee5afcae679c152
justkelvn <olowodepraise@gmail.com>
2022-12-13 11:03:09 -0800

    im federal agent Jack Bauer

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * file: main.h
+ * Description: contains all functions prototype used in this project
+ */
 
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,6 +19,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(char c);
 
 #endif
