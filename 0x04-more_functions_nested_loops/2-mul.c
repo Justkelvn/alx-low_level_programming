c3c6cbcacd8b89fecd49323d6ac4cbe43426e317
justkelvn <olowodepraise@gmail.com>
2022-12-15 17:12:57 -0800

    Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,19 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Print lowercase alphabet 10 times
+ * mul - multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplication
  */
 
-void print_alphabet_x10(void)
+int mul(int a, int b)
 {
-	char num, letter;
-
-	for (num = 0; num <= 9; num++)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);
-		}
-		_putchar('\n');
-	}
+	return (a * b);
 }
