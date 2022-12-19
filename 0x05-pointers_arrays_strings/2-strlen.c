291b43aa96d67c1f99845f5c1a973cc15a06b2b5
justkelvn <olowodepraise@gmail.com>
2022-12-19 02:33:18 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
