e6805a8dc95e717f156f1eb0adfc32e5079a62d0
justkelvn <olowodepraise@gmail.com>
2023-01-04 14:03:14 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -26,3 +26,23 @@ int find_sqrt(int num, int root)
 
 	return (find_sqrt(num, root + 1));
 }
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square toor of.
+ *
+ * Return: if n has a natural square root - the natural square root of n.
+ * if n does not have a natural square root - -1.
+ */
+int_sqrt_recursion(int n)
+{
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
+}
